c91ad4f125fa755eaa8e74e0fd2d492369727a66
Nathan Robertus <nathan.robertus@aed.pro>
2017-06-16 08:52:17 -0600

    Updated the python file to support multithreading and a proper heartbeat.

--- randy.py
+++ randy.py
@@ -1,23 +1,53 @@
 #!/usr/bin/env python
 
 import sqlite3
-import sched, time
+import os
+import time
+import datetime
+import threading
+import random
+from time import sleep
+from datetime import timedelta
+import smtplib
 
-conn=sqlite3.connect('randy.db')
+conn=sqlite3.connect('randy.db', check_same_thread=False)
 curs=conn.cursor()
-s = sched.scheduler(time.time, time.sleep)
 
 
-def do_something(sc): 
-    print "Heart beating"
-    curs.execute("""INSERT INTO heartbeat values(date('now'), time('now'), 'Healthy')""");
-    conn.commit();
-    # do your stuff
-    s.enter(60, 1, do_something, (sc,))
 
-s.enter(60, 1, do_something, (s,))
-s.run()
-	
+def heartbeat():
+    while True:
+        print "Heart beating"
+        curs.execute("""INSERT INTO heartbeat values(date('now'), time('now'), 'Healthy')""");
+        conn.commit();
+        time.sleep(10);
+
+def gpio(): # Use this for sensing wheel rotations.
+    while True:
+        if(){ #If rotation is detected
+            #curs.execute("""INSERT INTO rotations values(date('now'), time('now'), 0)""");
+            #conn.commit();
+        }
+        time.sleep(0.5)
+
+
+heartbeatthread = threading.Thread(target=heartbeat)
+gpiothread = threading.Thread(target=gpio)
+
+threads = [heartbeatthread, gpiothread]
+
+heartbeatthread.start()
+gpiothread.start()
+
+while True: # Master loop
+    for thread in threads:
+        if not thread.isAlive():
+            print "Caught a thread, restarting"
+            thread.start()
+    time.sleep(5 * 60) # every five minutes, restart all dead threads.
+
+
+
 
 
 #print "\nEntire database contents:\n"
