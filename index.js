8c9246068b25906c9ef1aee6d873c205fcb395e3
nrobertus <nathan.robertus@gmail.com>
2017-06-18 23:55:09 -0600

    nodejs server now behaving nicely

--- index.js
+++ index.js
@@ -6,24 +6,15 @@ var connection = mysql.createConnection({
   database : 'randy'
 });
 
-connection.connect()
-
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/heartbeat', function (req, res) {
   connection.query('SELECT * FROM heartbeat', function (err, rows, fields) {
-  if (err) throw err
-
-  res.send('The solution is: ', rows[0].solution)
-  })
+    res.send(rows);
+  });
 })
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!')
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 })
-
-
-
-//connection.end()
