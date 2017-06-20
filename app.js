4e4c5d681358ca5068392b5fcbaafd5c377eef39
Nathan Robertus <nathan.robertus@aed.pro>
2017-06-20 13:03:38 -0600

    Updated access control allow origin headers

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ const app = express();
 ///////////////////////////
 app.use(function(req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://randythehamster.com');
+  var allowedOrigins = ['http://randythehamster.com', 'http://www.randythehamster.com'];
+  var origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
