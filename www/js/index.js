4a3f5f741c6348d616257fd06d758796f7144440
Nathan Robertus <nathan.robertus@aed.pro>
2017-06-20 09:46:53 -0600

    Added support for both rotations and heartbeat to the client js

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   getHeartbeatCount();
+  getRotationsCount();
 });
 
 function getHeartbeatCount() {
@@ -12,7 +13,20 @@ function getHeartbeatCount() {
         updateUIElement("rotations-value", res[0].count);
       }
     });
-  }, 10000);
+  }, 1000);
+}
+
+function getRotationsCount() {
+  setInterval(function() {
+    $.ajax({
+      url: "http://69.145.60.173:3000/rotations/today/count",
+      type: 'GET',
+      dataType: 'json',
+      success: function(res) {
+        updateUIElement("distance-value", res[0].count);
+      }
+    });
+  }, 1000);
 }
 
 function updateUIElement(id, value) {
