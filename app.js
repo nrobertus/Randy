f89b7cbdc556d008098fd8924bc904c63d400480
Nathan Robertus <nathan.robertus@gmail.com>
2017-06-20 19:17:01 -0600

    Changed JSON format

--- app.js
+++ app.js
@@ -59,13 +59,13 @@ app.post('/google', function(req, res) {
     }]);
     */
   });
-  return res.json([{
+  return res.json({
     speech: "Today, Randy has run 35 rotations.",
     displayText: "Today, Randy has run 35 rotations.",
     data: "",
     contextOut: "",
     source: "www.randythehamster.com"
-  }]);
+  });
 });
 
 
