b2ae85c88edd8949f24e06675c4a6430be5f6d61
Nathan Robertus <nathan.robertus@gmail.com>
2017-06-20 22:44:27 -0600

    Updated constant name

--- app.js
+++ app.js
@@ -28,20 +28,20 @@ const app = express();
 ///////////////////////////
 
 exports.yourAction = (request, response) => {
-  const app = new App({request, response});
+  const google_app = new App({request, response});
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
 
   // Fulfill action business logic
   function responseHandler (app) {
     // Complete your fulfillment logic and send a response
-    app.ask('Hello, World!');
+    google_app.ask('Hello, World!');
   }
 
   const actionMap = new Map();
-  actionMap.set('<API.AI_action_name>', responseHandler);
+  actionMap.set('aboutRandy', responseHandler);
 
-  app.handleRequest(actionMap);
+  google_app.handleRequest(actionMap);
 };
 
 ////////////////////////////////
