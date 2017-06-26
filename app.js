3d0386bcacc4765a79b0e9ade33b61edb3a5df5d
Nathan Robertus <nathan.robertus@aed.pro>
2017-06-26 15:11:24 -0600

    Added debug line for heartbeat date posting

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ app.post('/heartbeat', function(req, res) {
   if (req.body.date) {
     date = req.body.date;
   }
+  console.log(sql);
   var sql = "INSERT INTO heartbeat (date, status) values(" + date + ", 'Healthy')";
   connection.query(sql, function(err, rows, fields) {
     res.send(rows);
