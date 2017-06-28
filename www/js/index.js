f484b49ecd8ba72d3396d9feae702280dd33d88e
Nathan Robertus <nathan.robertus@gmail.com>
2017-06-27 22:56:15 -0600

    Changed another animation option

--- www/js/index.js
+++ www/js/index.js
@@ -130,8 +130,6 @@ $(document).ready(function() { // TODO swap those out
   sseSubscribe(BASE_URL + 'rotations/weekday', updateRotations);
   sseSubscribe(BASE_URL + 'heartbeat/latest', updateHeartbeat);
   window.odometerOptions = {
-    duration: 6000, // Change how long the javascript expects the CSS animation to take
-    animation: 'count' // Count is a simpler animation method which just increments the value,
-    // use it when you're looking for something more subtle.
+    duration: 6000 // Change how long the javascript expects the CSS animation to take
   };
 });
