d9afa08ccffcee8ffa87e2177f03556c2c51420f
Nathan Robertus <nathan.robertus@aed.pro>
2017-06-29 11:56:09 -0600

    Removed danger stuff. Be safe, kids

--- app.js
+++ app.js
@@ -199,12 +199,6 @@ app.get('/uptime', function(req, res) {
   });
 });
 
-app.post('/command', function(req, res) {
-  executeCommand(req.body.command, function(data) {
-    res.send(data);
-  })
-});
-
 app.get("/logs", function(req, res) {
   fs.readFile(LOG_DIRECTORY, function read(err, data) {
     if (err) {
